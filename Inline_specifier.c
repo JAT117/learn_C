57550b7d9697679807a12ff2f807ca2582780682
JkTo7 <jtodd1158@gmail.com>
2021-02-10 10:13:47 -0600

    Update Inline_specifier.c

--- Inline_specifier.c
+++ Inline_specifier.c
@@ -1,9 +1,11 @@
 //Inline Specifier for Functions in C
 
 /*
-Inline functions are thoe whose definitions are small and be substituted at the place where the function call happened.
+Inline functions are those whose definitions are small and can be substituted at the place where the function call happened.
 Provides a hint to the compiler to perform function inlining(optimization). 
 Function substitution is a decided by the compiler and may be ignored.
+
+'functions with tiny definitions can be optimized, hope the compiler takes the hint'
 
 Supported by C99 but not earlier variants like K&R
 
